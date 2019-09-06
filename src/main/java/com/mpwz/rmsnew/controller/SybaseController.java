f8c813bd2be5168927678973af9fd285a781b525
Arpitpatodi123 <arpitpatodi97@gmail.com>
2019-09-06 17:26:55 +0530

    Controller Change

--- src/main/java/com/mpwz/rmsnew/controller/SybaseController.java
+++ src/main/java/com/mpwz/rmsnew/controller/SybaseController.java
@@ -6,11 +6,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import com.mpwz.rmsnew.service.sybase.SybaseService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 //@RequestMapping(value = "sybase")
 public class SybaseController
 {
